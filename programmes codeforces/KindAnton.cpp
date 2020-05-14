d11c0a9db0013e2d6443e8f06e7ece766d5f88fb
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2020-05-15 01:11:38 +0600

    cont dev 3

---
+++ programmes codeforces/KindAnton.cpp
@@ -0,0 +1,116 @@
+//                      Name: 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lli long long int //lld
+#define ulli unsigned long long int //llu
+
+#define pt cout<<"*"<<endl;
+#define nl cout<<endl;
+#define deb(x) cout << #x << "=" << x << endl;
+
+#define pb(a)    emplace_back(a)
+#define all(x)   (x).begin(),(x).end()
+#define rSort(x) sort((x).rbegin(),(x).rend())
+
+#define Sourav ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+//template<typename... T>void read(T&... args) {((cin >> args), ...);}
+template <class T>inline void sarray(T* st, T* nd){while(st<nd)cin>>*st++;/*sf("%d", st++);*/}
+template <class T>inline void parray(T* st, T* nd){while(st<nd)cout<<*st++<<endl;/*sf("%d", st++);*/}
+
+int main()
+{
+    Sourav;
+ #ifndef ONLINE_JUDGE
+            freopen("C:\\Users\\my_code\\input.in", "r", stdin); 
+            freopen("C:\\Users\\my_code\\output.in", "w", stdout);
+ #endif
+
+
+ int t;
+ cin>>t;
+    while(t--)
+    {
+      lli n;
+      cin>>n;
+      int ar[n+1]={0},arr[n+1]={0};
+      bool yes=true;
+    //   sarray(ar,ar+n);
+    //   sarray(arr,arr+n);
+    int f1=-1;
+int f2=-1;
+for (int i = 1; i <= n; i++)
+{
+    cin>>ar[i];
+    if(ar[i]==1&&f1==-1)f1=i;
+    if(ar[i]==-1&&f2==-1)f2=i;
+   
+
+}
+
+
+for (int i = 1; i <= n; i++)
+{
+    cin>>arr[i];
+    if(arr[i]>ar[i]&&(f1>=i||f1==-1))yes=false;
+    if(arr[i]<ar[i]&&(f2>=i||f2==-1))yes=false;
+}
+if(ar[1]!=arr[1])yes=false;
+cout<<(yes?"YES":"NO")<<endl;
+
+
+
+
+    }  
+
+
+
+
+  return 0;
+}
+
+/*
+  Documentation
+  5
+3
+1 -1 0
+1 1 -2
+3
+0 1 1
+0 2 2
+2
+1 0
+1 41
+2
+-1 0
+-1 -41
+5
+0 1 -1 1 -1
+1 1 -1 1 -1 
+*/
+/*
+map<lli, bool>m;
+  m[1000000000000000000] = 1;
+
+
+  // loop 
+  for(auto n:arrayname/anything)
+  {
+  
+  }
+Number of digits in N =floor(log10(N)) + 1;
+n = n << 1;   // Multiply n with 2  use >> for div
+*/
+
+/*
+int power_x(int x, unsigned y)
+{
+    if(y==0)
+        return 1;
+    if(!(y&1))
+        return power_x(x,y>>1)*power_x(x,y>>1);
+    return x*power_x(x,y>>1)*power_x(x,y>>1);
+}
+
+*/
